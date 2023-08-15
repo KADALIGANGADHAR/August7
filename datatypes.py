6b37ca002d61d5f0508117f43c5943d5a3e8a5db
kadali gangadhar <141652529+KADALIGANGADHAR@users.noreply.github.com>
2023-08-15 22:00:54 +0530

    Add files via upload

---
+++ datatypes.py
@@ -0,0 +1,25 @@
+a=1
+a=-1333
+b=1.322
+c=-2.637
+d=True
+e=False
+f=7+4+3j
+print(f.real)
+print(f.imag)
+
+
+#type conversion
+
+#a=75.647546
+
+# int-----> float
+
+#print(int(a))
+
+
+#a=int(input( "Enter the a value:"))
+#b=int(input("Enter the b value:"))
+#print(a+b) 
+
+
